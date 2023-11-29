b5561979c57cea2edfeb4be0d0247054266c685a
Zantiago-Bl <ysvega.v@gmail.com>
2023-11-29 17:06:19 -0500

    Mensaje nùmero 2

--- src/main/java/com/example/mode/controllers/AuthControllers.java
+++ src/main/java/com/example/mode/controllers/AuthControllers.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.mode.dao.PyUserDao;
 import com.example.mode.dao.UserDao;
+import com.example.mode.models.PyUser;
 import com.example.mode.models.User;
 import com.example.mode.utils.JWTUtil;
 
@@ -18,10 +20,13 @@ public class AuthControllers {
 	private UserDao userDao;
 	
 	@Autowired
+	private PyUserDao pyUserDao;
+	
+	@Autowired
 	private JWTUtil jwtUtil;
 
 	@RequestMapping(value = "api/login", method = RequestMethod.POST)
-	public String login(@RequestBody User user) {
+	public String login(@RequestBody User user, @RequestBody PyUser pyUser) {
 		
 		User userLog = userDao.verification(user);
 		if (userLog != null) {
@@ -30,7 +35,27 @@ public class AuthControllers {
 			
 			return tokenJwt;
 		}
+		
+		PyUser pyUserLog = pyUserDao.pyVerification(pyUser);
+		if (pyUserLog != null) {
+			
+			String tokenJwt = jwtUtil.create(String.valueOf(pyUserLog.getPyId()), pyUserLog.getPyNit());
+			
+			return tokenJwt;
+		}
 		return "Fail";
 	}
 	
+//	@RequestMapping(value = "api/login", method = RequestMethod.POST)
+//	public String pyLogin(@RequestBody PyUser pyUser) {
+//		
+//		PyUser pyUserLog = pyUserDao.pyVerification(pyUser);
+//		if (pyUserLog != null) {
+//			
+//			String tokenJwt = jwtUtil.create(String.valueOf(pyUserLog.getPyId()), pyUserLog.getPyNit());
+//			
+//			return tokenJwt;
+//		}
+//		return "Fail";
+//	}
 }
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,9 +1,51 @@
-$(document).ready(function() {
+//Animación login
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+// ----------------------------------------------
+
+$(document).ready(function() {
 
 });
 
+async function pyLogin() {
+	
+	let data = {	};
+	data.pyNit = document.getElementById('txtNit').value;
+	data.pyPassword = document.getElementById('txtPassword').value;
+	
+  const request = await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+     body: JSON.stringify(data)
+  });
+  
+  const response = await request.text();
+    if (response != "Fail"){
+		localStorage.token = response;
+		localStorage.email = data.pyNit;
+		window.location.href = "pyusers.html";
+	} else {
+		alert ("Inicio de sesión incorrecto. Inténtelo nuevamente.")
+	}
+
+};
+
 async function login() {
+	
+	console.log("Hola");
 	
 	let data = {	};
 	data.mail = document.getElementById('txtEmail').value;
